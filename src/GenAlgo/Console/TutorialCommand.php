659377988906e47700a715d6398688acab4d36a5
matthias <matthias@ich-digital.de>
2016-09-07 10:44:21 +0200

    updated todos

--- src/GenAlgo/Console/TutorialCommand.php
+++ src/GenAlgo/Console/TutorialCommand.php
@@ -43,7 +43,7 @@ class TutorialCommand extends Command
         require_once "$sourceRootPath/SimpleDemo.php";
         runSimpleDemo(42, 10, ConfigurationValues::fromArray([]));
 
-        // @todo: mad654: fetch all defaults from .env
-        // @todo: mad654: overwrite all config by command line
+        // @todo: mad654: fetch all from from .env (require searched and testcount)
+        // @todo: mad654: document configuration options in readme
     }
 }
