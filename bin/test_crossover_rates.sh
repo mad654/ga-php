ebb572a4f7e3a3e652c4a2c7730f8d43437249d9
matthias <matthias@ich-digital.de>
2016-09-16 23:46:22 +0200

    made other scripts parallel

--- bin/test_crossover_rates.sh
+++ bin/test_crossover_rates.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-function calc() { 
-	awk "BEGIN { print "$*" }" 
+function calc() {
+	awk "BEGIN { print "$*" }"
 }
 
-# test from 0.0009 bis 0.9 > Faktor 10000 > 9 - 9000
-for ACT in {9..9000}
-do
-	RATE=$(calc $ACT/10000)
-	TARGET_NUMBER=21 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-	TARGET_NUMBER=84 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-done
+function run() {
+	RATE=$(calc $1/1000000)
+	echo "Running for number $1 means crossoverRate: $RATE"
+	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=0.09375 CROSSOVER_RATE=$RATE bin/app gen-algo:tutorial -q
+}
+
+export -f run
+export -f calc
+
+# test from 0.09 bis 0.5 > Faktor 1000 > 900 - 500
+parallel -j -2 run ::: {900::500}
--- bin/test_mutation_rates.sh
+++ bin/test_mutation_rates.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-function calc() { awk "BEGIN { print "$*" }" }
+
+function calc() {
+    awk "BEGIN { print "$*" }"
+}
+
+function run() {
+	RATE=$(calc $1/1000000)
+	echo "Running for number $1 means mutationRate: $RATE"
+	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=$RATE bin/app gen-algo:tutorial -q
+}
+
+export -f run
+export -f calc
 
 # test from 0.000001 bis 0.01 > Faktor 1000000 > 1 - 10000
-for ACT in {1..10000}
-do
-	RATE=$(calc $ACT/1000000)
-	TARGET_NUMBER=21 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-	TARGET_NUMBER=84 TEST_COUNT=100 MUTATION_RATE=$RATE make run
-done
+parallel -j -2 run ::: {1..10000}
