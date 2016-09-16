7c8e00f34f9309f4e01ebe0183c44dc5cf2cfaa8
matthias <matthias@ich-digital.de>
2016-09-16 20:30:42 +0200

    improved testing commands

--- bin/test_crossover_rates.sh
+++ bin/test_crossover_rates.sh
@@ -2,10 +2,10 @@
 
 function calc() { awk "BEGIN { print "$*" }" }
 
-# test from 0.000001 bis 0.01 > Faktor 1000000 > 1 - 10000
-for ACT in {1..10000}
+# test from 0.0009 bis 0.9 > Faktor 10000 > 9 - 9000
+for ACT in {9..9000}
 do
-	RATE=$(calc $ACT/1000000)
+	RATE=$(calc $ACT/10000)
 	TARGET_NUMBER=21 TEST_COUNT=100 MUTATION_RATE=$RATE make run
 	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=$RATE make run
 	TARGET_NUMBER=84 TEST_COUNT=100 MUTATION_RATE=$RATE make run
---
+++ bin/test_numbers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function run() {
+	echo "Running for number $1"
+	TARGET_NUMBER=$1 TEST_COUNT=100 MUTATION_RATE=0.093750 bin/app gen-algo:tutorial -q
+}
+
+export -f run
+parallel -j -2 run ::: {1..10000}
