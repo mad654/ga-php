1b31c5a6a9f6987b0a2d6d6c512f26b660676ebb
matthias <matthias@ich-digital.de>
2016-09-23 22:16:18 +0200

    adjusted test scripts to current state

--- bin/test_mutation_rates.sh
+++ bin/test_mutation_rates.sh
@@ -8,9 +8,9 @@ function calc() {
 function run() {
     echo "Starting factor $1"
 
-	RATE=$(calc $1/1000000)
-	echo "Finished for number $1 means mutationRate: $RATE"
-	TARGET_NUMBER=42 TEST_COUNT=100 MUTATION_RATE=$RATE bin/app gen-algo:tutorial -q
+        RATE=$(calc $1/1000000)
+        echo "Finished for number $1 means mutationRate: $RATE"
+        TARGET_NUMBER=42 TEST_COUNT=10 MUTATION_RATE=$RATE bin/app gen-algo:tutorial -q
 }
 
 export -f run
@@ -18,4 +18,5 @@ export -f calc
 
 # test from 0.000001 bis 0.01 > Faktor 1000000 > 1 - 10000
 #parallel -j -2 run ::: {1..10000}
-parallel -j -2 run ::: {10000..1}
+parallel -j -2 run ::: {4750..1}
+
